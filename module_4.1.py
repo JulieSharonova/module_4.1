8a4e1af8c13e0de69a057eee86069c0b7f5410f0
JulieSharonova <sjulia.3009@gmail.com>
2024-08-13 17:26:55 +0300

    Add files via upload

---
+++ module_4.1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fake_math_d
+from true_math import divide as true_math_d
+
+result1 = true_math_d(69, 3)
+result2 = fake_math_d(3, 0)
+result3 = true_math_d(49, 7)
+result4 = true_math_d(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
